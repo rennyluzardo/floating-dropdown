fa3dd01c986ff0cfda3ecc0b5f4f5ab209f9843e
Renny <rluzardo@sidevox.com>
2020-05-14 17:26:27 -0500

    apointing to develcontactweb

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <script type="text/javascript" src="https://dashboard.contactvox.com/js/3.0/webcontact.floating-menu.js?fe01ce2a7fbac8fafaed7c982a04e229"></script>
+        <script type="text/javascript" src="https://develcontactweb.contactvox.com/js/3.0/webcontact.floating-menu.js?fe01ce2a7fbac8fafaed7c982a04e229"></script>
       </Head>
     </div>
   );
