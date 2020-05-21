e96d7f2e929f94e3437deaf0ac66f418c05d8776
Renny <rluzardo@sidevox.com>
2020-05-21 17:12:32 -0500

    apointing to tesiscontactweb

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <script type="text/javascript" src="https://develcontactweb.contactvox.com/js/3.0/webcontact.floating-menu.js?efbe310ccc7545052b59b874ed660fa2"></script>
+        <script type="text/javascript" src="https://tesiscontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?efbe310ccc7545052b59b874ed660fa2"></script>
       </Head>
     </div>
   );
