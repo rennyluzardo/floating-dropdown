231b0bd1d79b1c541eb3762fd6b60dc702c1391b
Renny <rluzardo@sidevox.com>
2020-05-21 17:53:29 -0500

    added stylesheets

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <link rel='stylesheet' type='text/css' href='https://awsdevelopment.contactvox.com/skin/4.0/efbe310ccc7545052b59b874ed660fa2/css/main.css' media='all' ></link>
         <script type="text/javascript" src="https://tesiscontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?efbe310ccc7545052b59b874ed660fa2"></script>
       </Head>
     </div>
