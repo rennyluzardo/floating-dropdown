862391e913b1d74e55bc2f1ab8a445ce889cbddd
Renny <rluzardo@sidevox.com>
2020-05-27 10:44:06 -0500

    apointing to dashboard.contactvox.com

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <link rel='stylesheet' type='text/css' href='https://awsdevelopment.contactvox.com/skin/4.0/efbe310ccc7545052b59b874ed660fa2/css/main.css' media='all' ></link>
-        <script type="text/javascript" src="https://tesiscontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?efbe310ccc7545052b59b874ed660fa2"></script>
+        <link rel='stylesheet' type='text/css' href='https://dashboard.contactvox.com/skin/4.0/efbe310ccc7545052b59b874ed660fa2/css/main.css' media='all' ></link>
+        <script type="text/javascript" src="https://develcontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?efbe310ccc7545052b59b874ed660fa2"></script>
       </Head>
     </div>
   );
