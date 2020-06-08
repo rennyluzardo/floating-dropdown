9f5d798160cc3bb32b9bc0b0b1bc8787b72f0314
Renny <rluzardo@sidevox.com>
2020-06-08 13:34:13 -0500

    apointing to amazon

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <link rel='stylesheet' type='text/css' href='https://dashboard.contactvox.com/skin/4.0/0c26fb08a93e4450c30f7a4bbb2a513a/css/main.css' media='all' ></link>
-        <script type="text/javascript" src="https://develcontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?0c26fb08a93e4450c30f7a4bbb2a513a"></script>
+        <link rel='stylesheet' type='text/css' href='https://awsdevelopment.contactvox.com/skin/4.0/efbe310ccc7545052b59b874ed660fa2/css/main.css' media='all' ></link>
+        <script type="text/javascript" src="https://develcontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?efbe310ccc7545052b59b874ed660fa2"></script>
       </Head>
     </div>
   );
