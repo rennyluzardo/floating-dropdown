0e896045d3c5f8bcb4163089aad3f38f19872087
Renny <rluzardo@sidevox.com>
2020-06-11 11:54:49 -0500

    apointing to hyundai

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <link rel='stylesheet' type='text/css' href='https://awsdevelopment.contactvox.com/skin/4.0/0c26fb08a93e4450c30f7a4bbb2a513a/css/main.css' media='all' ></link>
-        <script type="text/javascript" src="https://develcontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?0c26fb08a93e4450c30f7a4bbb2a513a"></script>
+        <link rel='stylesheet' type='text/css' href='https://awsdevelopment.contactvox.com/skin/4.0/2d404fcb3bb4b7c1b1ef685f38214ac2/css/main.css' media='all' ></link>
+        <script type="text/javascript" src="https://develcontactweb.contactvox.com/js/3.0/webcontact.floating-menu.min.js?2d404fcb3bb4b7c1b1ef685f38214ac2"></script>
       </Head>
     </div>
   );
